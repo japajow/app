6cf10260d295ca41005b1e1c10eeb8a2b2a679bb
japajow <sitewnkcorporate@gmail.com>
2023-08-13 16:31:42 +0900

    add package axios

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 import { createApp } from 'vue'
-import App from './App.vue';
+import App from '@/App.vue';
 
 import '@/assets/css/tailwind.css'
-import router from './router'
-import store from './store'
+import router from '@/router'
+import store from '@/store'
+import '@/plugins/axios'
 
 createApp(App).use(store).use(router).mount('#app')
